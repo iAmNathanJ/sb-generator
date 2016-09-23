33ac069c34dd3590bfc049ea8f13657c68cc41e8
iAmNathanJ <jacobs@heysparkbox.com>
2016-09-22 22:29:00 -0400

    Feat: update file scaffolding
    - create data file
    - create js file
    - update main scss file

--- generators/pattern/index.js
+++ generators/pattern/index.js
@@ -1,24 +1,38 @@
 'use strict';
+var path = require('path');
+var fs = require('fs');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
+// var yosay = require('yosay');
+var _ = require('lodash');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    // this.log(yosay(
-    //   'Let\'s make a pattern with the ' + chalk.red('generator-sb-pattern') + ' generator!'
-    // ));
+  // note: arguments and options should be defined in the constructor.
+  constructor: function () {
+    yeoman.Base.apply(this, arguments);
 
+    // This makes `appname` a required argument.
+    this.argument('patternName', {type: String, required: true});
+    // And you can then access it later on this way; e.g. kebab-cased
+    this.patternName = _.kebabCase(this.patternName);
+    this.title = _.startCase(this.patternName);
+  },
+
+  prompting: function () {
     var prompts = [{
-      type: 'input',
-      name: 'patternName',
-      message: 'Pattern name?',
-      default: ''
+      type: 'confirm',
+      name: 'jsFile',
+      message: 'Include JS file?',
+      default: false
+    }, {
+      type: 'confirm',
+      name: 'dataFile',
+      message: 'Include data file?',
+      default: false
     }];
 
     return this.prompt(prompts).then(function (props) {
-      this.destinationRoot('./src/templates/partials');
+      this.destinationRoot('./src');
       // To access props later use this.props.someAnswer;
       this.props = props;
     }.bind(this));
@@ -27,21 +41,47 @@ module.exports = yeoman.Base.extend({
   writing: function () {
     this.template(
       this.templatePath('pattern.hbs'),
-      this.destinationPath(`${this.props.patternName}/_${this.props.patternName}.hbs`),
-      this.props
+      this.destinationPath(`templates/partials/${this.patternName}/_${this.patternName}.hbs`),
+      this
     );
 
     this.template(
       this.templatePath('pattern.scss'),
-      this.destinationPath(`${this.props.patternName}/_${this.props.patternName}.scss`),
-      this.props
+      this.destinationPath(`templates/partials/${this.patternName}/_${this.patternName}.scss`),
+      this
     );
 
     this.template(
       this.templatePath('README.md'),
-      this.destinationPath(`${this.props.patternName}/README.md`),
-      this.props
+      this.destinationPath(`templates/partials/${this.patternName}/README.md`),
+      this
     );
+
+    if (this.props.jsFile) {
+      this.template(
+        this.templatePath('pattern.js'),
+        this.destinationPath(`templates/partials/${this.patternName}/${this.patternName}.js`),
+        this
+      );
+    }
+
+    if (this.props.dataFile) {
+      this.template(
+        this.templatePath('pattern.yml'),
+        this.destinationPath(`data/${this.patternName}.yml`),
+        this
+      );
+    }
+
+    var scssMainPath = path.resolve('scss/main.scss');
+    var scssMainContent = this.read(scssMainPath);
+    var newScss = `@import "${this.patternName}/${this.patternName}";`;
+    var msg = `\n${scssMainContent}${chalk.bold(newScss)}\n`;
+    var newScssContent = `${scssMainContent}${newScss}\n`;
+    fs.writeFile(scssMainPath, newScssContent, () => {
+      this.log(msg);
+      this.log(chalk.green('main.scss updated'));
+    });
   },
 
   install: function () {
